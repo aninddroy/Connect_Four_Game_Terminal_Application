a0175c0f68402677c4022f2af9345eeaab40133a
aninddroy <aninddroy@yahoo.com>
2023-07-19 10:58:00 +0530

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -18,135 +18,53 @@
 /* Author - Anind Duttaroy */
 /*******************************************************************/
 
-#include <cstdlib>
+#include <string>
+#include <random>
+
 #include "Board.h"
+
 
 namespace connect_four
 {
 	Board::Board()
-		: result(player_and_result::game_not_complete),
-		  human_player(player_and_result::yellow),
-		  machine_player(player_and_result::red),
-		  turn(player_and_result::yellow),
-		  human_entered_column_number(0),
-		  machine_entered_column_number(0),
-		  quit_game_or_continue('c'), 
-		  game_complete(false)
+		: human_player(player::yellow),
+		  machine_player(player::red),
+		  turn(player::yellow),
+		  winner(player::yellow),
+		  result(result::game_not_complete),
+		  game_complete(false),
+		  game_quitted(false)
 	{
 	}
 
-	bool Board::is_game_complete()
+	bool Board::IsGameComplete() const
 	{
 		return game_complete;
 	}
 
-	player_and_result Board::get_result()
+	bool Board::IsGameQuitted() const
+	{
+		return game_quitted;
+	}
+
+	result Board::GetResult() const
 	{
 		return result;
 	}
 
-	void Board::set_board(const std::vector<std::vector<char>>& source_board)
+	player Board::GetWinner() const
 	{
-		for (size_t i = 0; i < board_rows; ++i)
-		{
-			for (size_t j = 0; j < board_columns; ++j)
-			{
-				board[i][j] = source_board[i][j];
-			}
-		}
-	}
-
-	void Board::set_game_complete_status(bool game_complete_status)
-	{
-		game_complete = game_complete_status;
-	}
-
-	void Board::take_human_input()
-	{
-		std::cout << std::endl << "Do you want to quit the game? Press c to continue or any other key to quit." << std::endl;
-		std::cin >> quit_game_or_continue;
-		if (quit_game_or_continue == 'c')
-		{
-			while (1)
-			{
-				std::cout << std::endl << "Enter column number between 0 and 6 inclusive." << std::endl;
-				std::cin >> human_entered_column_number;
-				if ((human_entered_column_number >= 0 && human_entered_column_number < board_columns) && 
-					check_if_empty_position(human_entered_column_number))
-					break;
-			}
-		}
-		else
-		{
-			game_complete = true;
-		}
-	}
-
-	void Board::generate_machine_input()
-	{
-		while (1)
-		{
-			machine_entered_column_number = 0 + (rand() % 6);
-			if (check_if_empty_position(machine_entered_column_number))
-				break;
-		}
-	}
-
-	bool Board::check_if_empty_position(int column_number)
-	{
-		for (int i = board_rows - 1; i >= 0; --i)
-		{
-			if (board[i][column_number] == 'W')
-				return true;
-		}
-		return false;
-	}
-
-	void Board::mark_position_on_board(char ch, int column_number)
-	{
-		for (int i = board_rows - 1; i >= 0; --i)
-		{
-			if (board[i][column_number] != 'W')
-				continue;
-			if (board[i][column_number] == 'W')
-			{
-				board[i][column_number] = ch;
-				break;
-			}
-		}
-	}
-
-	void Board::select_position_on_board()
-	{
-		if ((turn == human_player) && (human_player == player_and_result::yellow))
-		{
-			mark_position_on_board('Y', human_entered_column_number);
-		}
-		else if ((turn == human_player) && (human_player == player_and_result::red))
-		{
-			mark_position_on_board('R', human_entered_column_number);
-		}
-		else if ((turn == machine_player) && (machine_player == player_and_result::yellow))
-		{
-			mark_position_on_board('Y', machine_entered_column_number);
-		}
-		else if ((turn == machine_player) && (machine_player == player_and_result::red))
-		{
-			mark_position_on_board('R', machine_entered_column_number);
-		}
+		return winner;
 	}
 
 	// Print the board
-	void Board::print_board()
+	void Board::PrintBoard() const
 	{
-		if (board.empty())
-			return;
-
 		std::cout << std::endl;
 
-		for (size_t i = 0; i < board_rows; ++i)
+		for (int i = 0; i < connect_four::board_rows; ++i)
 		{
-			for (size_t j = 0; j < board_columns; ++j)
+			for (int j = 0; j < connect_four::board_columns; ++j)
 			{
 				std::cout << " " << board[i][j] << " ";
 			}
@@ -156,29 +74,130 @@ namespace connect_four
 	}
 
 
+	int Board::TakeHumanInput()
+	{
+		int human_entered_column_number = 0;
+
+		std::cin >> human_entered_column_number;
+		if ( !(human_entered_column_number >= board_column_num_min
+			&& human_entered_column_number <= board_column_num_max) 
+			//the column number is reduced by 1, since column indexing starts from 0
+			|| !CheckIfEmptyPosition(human_entered_column_number - 1) )
+		{
+			game_quitted = true;
+		}
+
+		return human_entered_column_number;
+	}
+
+
+	int Board::GenerateMachineInput()
+	{
+		int machine_entered_column_number = 0;
+		while (1)
+		{
+			//experimenting with two options for generating random numbers
+			
+			//machine_entered_column_number = 1 + (rand() % 7);
+			
+			std::uniform_int_distribution<> temp{ 1, 7 };
+			std::default_random_engine engine;
+			machine_entered_column_number = temp(engine);
+
+			//the column number is reduced by 1, since column indexing starts from 0
+			if (CheckIfEmptyPosition(machine_entered_column_number - 1))
+				break;
+		}
+
+		return machine_entered_column_number;
+	}
+
+
+	bool Board::SetPositionAndCheck(player turn, int column_number)
+	{
+		bool position_marked = MarkPositionOnBoard(turn, column_number);
+		if (!position_marked)
+		{
+			return position_marked;
+		}
+
+		PrintBoard();
+		CheckForConnectedDisks();
+
+		return position_marked;
+	}
+
+
+	bool Board::CheckIfEmptyPosition(int column_number) const
+	{
+		for (int i = board_rows - 1; i >= 0; --i)
+		{
+			if (board[i][column_number] == 'W')
+				return true;
+		}
+		return false;
+	}
+
+	bool Board::MarkPositionOnBoard(player turn, int column_number)
+	{
+		bool valid_position = false;
+		char ch = yellow;
+
+		if (turn == player::yellow)
+		{
+			ch = yellow;
+		}
+		else if (turn == player::red)
+		{
+			ch = red;
+		}
+
+		for (int i = board_rows - 1; i >= 0; --i)
+		{
+			if (board[i][column_number] != white)
+				continue;
+			if (board[i][column_number] == white)
+			{
+				board[i][column_number] = ch;
+				valid_position = true;
+				break;
+			}
+		}
+
+		return valid_position;
+	}
+
 	// Check if the board contains four connected disks either in a row, or,
 	// either, horizontally, vertically, or diagonally
-	void Board::check_for_connected_disks()
+	void Board::CheckForConnectedDisks()
 	{
-		result = player_and_result::game_not_complete;
+		result = result::game_not_complete;
 
-		for (size_t i = 0; i < board_rows; ++i)
+		std::pair<int, int> first_position;
+		std::pair<int, int> second_position;
+		std::pair<int, int> third_position;
+		std::pair<int, int> fourth_position;
+
+
+		for (int i = 0; i < board_rows; ++i)
 		{
-			for (size_t j = 0; j < board_columns; ++j)
+			for (int j = 0; j < board_columns; ++j)
 			{
 				//Check the horizontal connections first
 				if (j + 3 < board_columns)
 				{
-					if (board[i][j] == 'Y' && board[i][j + 1] == 'Y' && board[i][j + 2] == 'Y' && board[i][j + 3] == 'Y')
+					first_position.first = i;
+					first_position.second = j;
+					second_position.first = i;
+					second_position.second = j + 1;
+					third_position.first = i;
+					third_position.second = j + 2;
+					fourth_position.first = i;
+					fourth_position.second = j + 3;
+
+					if (CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, yellow)
+					  || CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, red) )
 					{
-						result = player_and_result::yellow;
-						game_complete = true;
-						break;
-					}
-					if (board[i][j] == 'R' && board[i][j + 1] == 'R' && board[i][j + 2] == 'R' && board[i][j + 3] == 'R')
-					{
-						result = player_and_result::red;
-						game_complete = true;
 						break;
 					}
 				}
@@ -186,16 +205,18 @@ namespace connect_four
 				//Check the vertical connections
 				if (i + 3 < board_rows)
 				{
-					if (board[i][j] == 'Y' && board[i + 1][j] == 'Y' && board[i + 2][j] == 'Y' && board[i + 3][j] == 'Y')
+					first_position.first = i;
+					first_position.second = j;
+					second_position.first = i + 1;
+					second_position.second = j;
+					third_position.first = i + 2;
+					third_position.second = j;
+					fourth_position.first = i + 3;
+					fourth_position.second = j;
+
+					if (CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, yellow)
+						|| CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, red))
 					{
-						result = player_and_result::yellow;
-						game_complete = true;
-						break;
-					}
-					if (board[i][j] == 'R' && board[i + 1][j] == 'R' && board[i + 2][j] == 'R' && board[i + 3][j] == 'R')
-					{
-						result = player_and_result::red;
-						game_complete = true;
 						break;
 					}
 				}
@@ -203,37 +224,39 @@ namespace connect_four
 				//Check the diagonal connections from bottom left to top right
 				if (i > 2 && j + 3 < board_columns)
 				{
-					if (board[i][j] == 'Y' && board[i - 1][j + 1] == 'Y' && board[i - 2][j + 2] == 'Y' && board[i - 3][j + 3] == 'Y')
-					{
-						result = player_and_result::yellow;
-						game_complete = true;
-						break;
-					}
-					if (board[i][j] == 'R' && board[i - 1][j + 1] == 'R' && board[i - 2][j + 2] == 'R' && board[i - 3][j + 3] == 'R')
-					{
-						result = player_and_result::red;
-						game_complete = true;
-						break;
-					}
+					first_position.first = i;
+					first_position.second = j;
+					second_position.first = i - 1;
+					second_position.second = j + 1;
+					third_position.first = i - 2;
+					third_position.second = j + 2;
+					fourth_position.first = i - 3;
+					fourth_position.second = j + 3;
 
+					if (CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, yellow)
+						|| CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, red))
+					{
+						break;
+					}
 				}
 
 				//Check the diagonal connections from top left to bottom right
 				if (i + 3 < board_rows && j + 3 < board_columns)
 				{
-					if (board[i][j] == 'Y' && board[i + 1][j + 1] == 'Y' && board[i + 2][j + 2] == 'Y' && board[i + 3][j + 3] == 'Y')
-					{
-						result = player_and_result::yellow;
-						game_complete = true;
-						break;
-					}
-					if (board[i][j] == 'R' && board[i + 1][j + 1] == 'R' && board[i + 2][j + 2] == 'R' && board[i + 3][j + 3] == 'R')
-					{
-						result = player_and_result::red;
-						game_complete = true;
-						break;
-					}
+					first_position.first = i;
+					first_position.second = j;
+					second_position.first = i + 1;
+					second_position.second = j + 1;
+					third_position.first = i + 2;
+					third_position.second = j + 2;
+					fourth_position.first = i + 3;
+					fourth_position.second = j + 3;
 
+					if (CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, yellow)
+						|| CheckConnectedDisksAndMarkWinner(first_position, second_position, third_position, fourth_position, red))
+					{
+						break;
+					}
 				}
 
 			}//end of inner for loop
@@ -249,14 +272,14 @@ namespace connect_four
 		//else it would be a draw
 		game_complete = true;
 		
-		for (size_t i = 0; i < board_rows; ++i)
+		for (int i = 0; i < board_rows; ++i)
 		{
-			for (size_t j = 0; j < board_columns; ++j)
+			for (int j = 0; j < board_columns; ++j)
 			{
-				if (board[i][j] == 'Y' || board[i][j] == 'R')
+				if (board[i][j] == yellow || board[i][j] == red)
 					continue;
 
-				if (board[i][j] == 'W')	//game not yet complete
+				if (board[i][j] == white)	//game not yet complete
 				{
 					game_complete = false;
 					break;
@@ -268,11 +291,37 @@ namespace connect_four
 		
 		//The game is complete and it is a draw
 		if (game_complete)
-			result = player_and_result::draw;
+			result = result::draw;
 
 	}
 
-	void Board::run_game()
+	bool Board::CheckConnectedDisksAndMarkWinner(const std::pair<int, int>& first_position, const std::pair<int, int>& second_position,
+		const std::pair<int, int>& third_position, const std::pair<int, int>& fourth_position, char color)
+	{
+		game_complete = false;
+
+		if ( (board[first_position.first][first_position.second] == color)
+			&& (board[second_position.first][second_position.second] == color)
+			&& (board[third_position.first][third_position.second] == color)
+			&& (board[fourth_position.first][fourth_position.second] == color))
+		{
+			if (color == yellow)
+			{
+				winner = player::yellow;
+			}
+			else if (color == red)
+			{
+				winner = player::red;
+
+			}
+			result = result::won;
+			game_complete = true;
+		}
+
+		return game_complete;
+	}
+
+	void Board::RunGame()
 	{
 		while (1)
 		{
@@ -280,69 +329,82 @@ namespace connect_four
 			std::cout << std::endl << "Choose a colour to play - Press Y or y for Yellow or R or r for Red." << std::endl;
 			std::cin >> ch;
 
-			if (ch == 'Y' || ch == 'y')
+			if (ch == yellow || ch == yellow_small)
 			{
-				human_player = player_and_result::yellow;
-				machine_player = player_and_result::red;
+				human_player = player::yellow;
+				machine_player = player::red;
 				break;
 			}
-			else if (ch == 'R' || ch == 'r')
+			else if (ch == red || ch == red_small)
 			{
-				human_player = player_and_result::red;
-				machine_player = player_and_result::yellow;
+				human_player = player::red;
+				machine_player = player::yellow;
 				break;
+			}
+			else
+			{
+				std::cout << std::endl << "Invalid input. " << std::endl;
 			}
 		}
 
 		turn = human_player;
 
+		std::cout << std::endl << "For your turn, enter column number between 1 and 7 inclusive." << std::endl;
+		std::cout << std::endl << "If you want to quit the game, at any point, press any other key, during your turn." << std::endl;
+
+
 		while (1)
 		{
-			if (turn == player_and_result::yellow)
+			bool position_marked = false;
+
+			if (turn == human_player)
 			{
-				if (human_player == player_and_result::yellow)
+				while (1)
 				{
-					take_human_input();
-				}
-				else
-				{
-					generate_machine_input();
+					int human_entered_column_number = TakeHumanInput();
+					if (IsGameQuitted())
+						break;
+
+					//the column number is reduced by 1, since column indexing starts from 0
+					position_marked = SetPositionAndCheck(turn, human_entered_column_number - 1);
+					if (!position_marked)
+					{
+						continue;
+					}
+					else
+					{
+						break;
+					}
 				}
 
-				if (quit_game_or_continue == 'c')
-				{
-					select_position_on_board();
-					print_board();
-					check_for_connected_disks();
-				}
-
-				if (is_game_complete())
+				if (IsGameQuitted())
 					break;
-				turn = player_and_result::red;
+
+				turn = machine_player;
 			}
-			else if (turn == player_and_result::red)
+			else if (turn == machine_player)
 			{
-				if (human_player == player_and_result::red)
+				while (1)
 				{
-					take_human_input();
-				}
-				else
-				{
-					generate_machine_input();
+					int machine_entered_column_number = GenerateMachineInput();
+					//the column number is reduced by 1, since column indexing starts from 0
+					position_marked = SetPositionAndCheck(turn, machine_entered_column_number - 1);
+
+					if (!position_marked)
+					{
+						continue;
+					}
+					else
+					{
+						break;
+					}
 				}
 
-				if (quit_game_or_continue == 'c')
-				{
-					select_position_on_board();
-					print_board();
-					check_for_connected_disks();
-				}
-
-				if (is_game_complete())
-					break;
-				turn = player_and_result::yellow;
+				turn = human_player;
 			}
 
+			if (IsGameComplete())
+				break;
 		}
 
 	}
